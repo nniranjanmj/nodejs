3d40af1edcb4efa7ce3b9dfb1aea6db2420c4e79
SANA-TASMIA <103491953+SANA-TASMIA@users.noreply.github.com>
2023-09-12 12:27:14 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,7 +7,7 @@ cd /home/ubuntu/server
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 
 #_Download_Node_&NPM
-yum -y install nodejs npm
+apt-get -y install nodejs npm
 
 #_Download_PM2
 npm install pm2@latest -g
